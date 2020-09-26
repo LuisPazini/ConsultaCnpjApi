3081402116732ab07ab5fcb3a29712797a9733c9
LUIS AUGUSTO BRUNELLI PAZINI <luis.pazini@fatec.sp.gov.br>
2020-09-26 09:44:39 -0300

    Criação da classe de consulta da ReceitaWS

---
+++ ConsultaCnpjApi/Resources/api/ConsultaReceitaWs.cs
@@ -0,0 +1,26 @@
+﻿using ConsultaCnpjApi.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace ConsultaCnpjApi.Services
+{
+    public class ConsultaReceitaWs
+    {
+        public async Task<Empresa> consultar(string cnpj)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(string.Format("https://receitaws.com.br/v1/cnpj/"));
+                var response = await client.GetAsync(cnpj);
+
+                string dados = await response.Content.ReadAsStringAsync();
+
+                Empresa empresa = new JavaScriptSerializer().Deserialize<Empresa>(dados);
+
+                return empresa;
+            }
+        }
+    }
+}
