773b545c77795e511ef88cbfeb6d158d30995405
LUIS AUGUSTO BRUNELLI PAZINI <luis.pazini@fatec.sp.gov.br>
2020-09-26 11:07:39 -0300

    Criação de classe para Commit

---
+++ ConsultaCnpjApi/Resources/api/ClasseParaCommit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConsultaCnpjApi.Resources.api
+{
+    public class ClasseParaCommit
+    {
+    }
+}
