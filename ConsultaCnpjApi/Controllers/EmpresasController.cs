4fd6fa8922eed6b7506207cc4fa3ccfadd08393d
LUIS AUGUSTO BRUNELLI PAZINI <luis.pazini@fatec.sp.gov.br>
2020-09-27 20:54:08 -0300

    Configurações dos métodos GET, POST e DELETE

--- ConsultaCnpjApi/Controllers/EmpresasController.cs
+++ ConsultaCnpjApi/Controllers/EmpresasController.cs
@@ -1,36 +1,48 @@
 ﻿using ConsultaCnpjApi.Models;
 using ConsultaCnpjApi.Resources.db;
 using ConsultaCnpjApi.Services;
+using Microsoft.Ajax.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web.Helpers;
 using System.Web.Http;
+using System.Web.UI.WebControls;
 
 namespace ConsultaCnpjApi.Controllers
 {
     public class EmpresasController : ApiController
     {
         // GET: api/Empresas
-        public IEnumerable<Empresa> Get()
+        public List<Empresa> Get()
         {
-            return null;
+            using(Contexto contexto = new Contexto())
+            {
+                List<Empresa> empresas = new List<Empresa>();
+                contexto.Empresas.ForEach(e => empresas.Add(e));
+
+                return empresas;
+            }
         }
 
-        // GET: api/Empresas/5
-        public async System.Threading.Tasks.Task<Empresa> GetAsync(string cnpj)
+        // GET: api/Empresas/{id}
+        public Empresa Get(int id)
         {
-                        
-            return null;
+            using(Contexto contexto = new Contexto())
+            {
+                Empresa empresa = contexto.Empresas.Find(id);
+                return empresa;
+            }
         }
 
         // POST: api/Empresas
-        public async System.Threading.Tasks.Task PostAsync([FromBody]string cnpj)
+        public async System.Threading.Tasks.Task<IHttpActionResult> PostAsync([FromBody] Empresa e)
         {
             // Consultando os dados na API da Receita
             ConsultaReceitaWs consulta = new ConsultaReceitaWs();
-            Empresa empresa = await consulta.Consultar(cnpj);
+            Empresa empresa = await consulta.Consultar(e.Cnpj);
 
             // Adicionando a empresa no Banco de Dados
             using (Contexto contexto = new Contexto())
@@ -38,26 +50,50 @@ namespace ConsultaCnpjApi.Controllers
                 contexto.Empresas.Add(new Empresa()
                 {
                     Id = empresa.Id,
+                    Atividade_Principal = empresa.Atividade_Principal,
                     Data_Situacao = empresa.Data_Situacao,
                     Tipo = empresa.Tipo,
                     Nome = empresa.Nome,
                     Uf = empresa.Uf,
                     Telefone = empresa.Telefone,
-                    Email = empresa.Email
+                    Email = empresa.Email,
+                    Atividades_Secundarias = empresa.Atividades_Secundarias,
+                    Qsa = empresa.Qsa,
+                    Situacao = empresa.Situacao,
+                    Bairro = empresa.Bairro,
+                    Logradouro = empresa.Logradouro,
+                    Numero = empresa.Numero,
+                    Cep = empresa.Cep,
+                    Municipio = empresa.Municipio,
+                    Porte = empresa.Porte,
+                    Abertura = empresa.Abertura,
+                    Natureza_Juridica = empresa.Natureza_Juridica,
+                    Fantasia = empresa.Fantasia,
+                    Cnpj = empresa.Cnpj,
+                    Ultima_Atualizacao = empresa.Ultima_Atualizacao,
+                    Status = empresa.Status,
+                    Complemento = empresa.Complemento,
+                    Efr = empresa.Efr,
+                    Motivo_Situacao = empresa.Motivo_Situacao,
+                    Situacao_Especial = empresa.Situacao_Especial,
+                    Data_Situacao_Especial = empresa.Data_Situacao_Especial,
+                    Capital_Social = empresa.Capital_Social,
+                    Extra = empresa.Extra,
+                    Billing = empresa.Billing
                 });
 
                 contexto.SaveChanges();
             }
+            return Ok();
         }
 
-        // PUT: api/Empresas/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE: api/Empresas/5
+        // DELETE: api/Empresas/{id}
         public void Delete(int id)
         {
+            using (Contexto contexto = new Contexto())
+            {
+                contexto.Empresas.Remove(contexto.Empresas.Find(id));
+            }
         }
     }
 }
---
+++ ConsultaCnpjApi/Migrations/202009272009409_Alteracoes na classe Empresa.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsultaCnpjApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AlteracoesnaclasseEmpresa : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Empresas", "Discriminator", c => c.String(nullable: false, maxLength: 128));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Empresas", "Discriminator");
+        }
+    }
+}
