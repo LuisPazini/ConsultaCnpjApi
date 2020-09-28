e0065efe1d6534b8ea9d5502a7f845df0e815580
LUIS AUGUSTO BRUNELLI PAZINI <luis.pazini@fatec.sp.gov.br>
2020-09-28 14:30:19 -0300

    Simplificação do método POST

--- ConsultaCnpjApi/Controllers/EmpresasController.cs
+++ ConsultaCnpjApi/Controllers/EmpresasController.cs
@@ -96,6 +96,8 @@ namespace ConsultaCnpjApi.Controllers
                 using (Contexto contexto = new Contexto())
                 {
                     contexto.Empresas.Remove(contexto.Empresas.Find(id));
+
+                    contexto.SaveChanges();
                 }
             }
             catch (Exception)
